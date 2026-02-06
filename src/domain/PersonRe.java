8af93b71ca7a414819eb95ba327df9e71f683f2d
guddlrdl123@gmail.com <guddlrdl123@gmail.com>
2026-02-06 18:18:38 +0900

    Record

---
+++ src/domain/PersonRe.java
@@ -0,0 +1,107 @@
+package domain;
+
+import java.security.Timestamp;
+
+// record 객체 선언 - class 대신에 record
+// 클래스 선언시에 없던 "()" 사용한다.
+// 1. 간결한 객체 정의
+// 2. 메서드 자동 생성
+// 3. 생성자 자동 생성(Builder 객체 사용)
+// 4. 불변성(***) - 객체 안에 있는 멤버변수들은 final의 속성을 가지고 있다.
+// getter 기본 설정.
+public record PersonRe(
+        // 멤버 변수 선언 위치
+        int id, String userId, String userPw, String userName,
+        String userEmail, String phone1, String phone2,
+        int age, String address1, String address2,
+        Timestamp regDate, Timestamp modifyDate) {
+    public Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+        private int id;
+        private String userId;
+        private String userPw;
+        private String userName;
+        private String userEmail;
+        private String phone1;
+        private String phone2;
+        private int age;
+        private String address1;
+        private String address2;
+        private Timestamp regDate;
+        private Timestamp modifyDate;
+
+        // 기본 생성자
+        public Builder() {
+        }
+
+        public Builder id(int id) {
+            this.id = id; // 생성된 객체 id값을 매개변개 id 대입
+            return this; // 객체를 넘기겠습니다.
+        }
+
+        public Builder userId(String userId) {
+            this.userId = userId;
+            return this;
+        }
+
+        public Builder userPw(String userPw) {
+            this.userPw = userPw;
+            return this;
+        }
+
+        public Builder userName(String userName) {
+            this.userName = userName;
+            return this;
+        }
+
+        public Builder userEmail(String userEmail) {
+            this.userEmail = userEmail;
+            return this;
+        }
+
+        public Builder phone1(String phone1) {
+            this.phone1 = phone1;
+            return this;
+        }
+
+        public Builder phone2(String phone2) {
+            this.phone2 = phone2;
+            return this;
+        }
+
+        public Builder age(int age) {
+            this.age = age;
+            return this;
+        }
+
+        public Builder address1(String address1) {
+            this.address1 = address1;
+            return this;
+        }
+
+        public Builder address2(String address2) {
+            this.address2 = address2;
+            return this;
+        }
+
+        public Builder regDate(Timestamp regDate) {
+            this.regDate = regDate;
+            return this;
+        }
+
+        public Builder modifyDate(Timestamp modifyDate) {
+            this.modifyDate = modifyDate;
+            return this;
+        }
+
+        // 메서드 : build()
+        public PersonRe build() {
+            return new PersonRe(id, userId, userPw, userName, userEmail, phone1,
+                    phone2, age, address1, address2, regDate, modifyDate);
+        }
+    }
+
+}
